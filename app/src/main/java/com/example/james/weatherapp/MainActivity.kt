07388a3b3545ad0d73b85e161ee53c054dde2712
James Mwangi <jamesmwangi55@gmail.com>
2017-05-30 18:34:02 +0300

    some changes

--- app/src/main/java/com/example/james/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/james/weatherapp/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.james.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "hello kotlin"
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
     }
+
 }
